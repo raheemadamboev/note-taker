5f92d8a70f1bb40db45b8d41e28074a806f0a0c4
Raheem <raheemideadev@gmail.com>
2021-10-25 20:34:46 +0500

    initial

---
+++ app/src/main/java/xyz/teamgravity/notetaker/MainActivity.kt
@@ -0,0 +1,23 @@
+package xyz.teamgravity.notetaker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import xyz.teamgravity.notetaker.ui.theme.NoteTakerTheme
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NoteTakerTheme {
+
+            }
+        }
+    }
+}
