568300512abc6f3a1a2ffb2bdf11172f3064f43c
Raheem <raheemideadev@gmail.com>
2021-11-04 21:52:21 +0500

    set up test runner

---
+++ app/src/androidTest/java/xyz/teamgravity/notetaker/feature_note/presentation/notes/NotesScreenTest.kt
@@ -0,0 +1,12 @@
+package xyz.teamgravity.notetaker.feature_note.presentation.notes
+
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
+import xyz.teamgravity.notetaker.injection.ApplicationModule
+
+@HiltAndroidTest
+@UninstallModules(ApplicationModule::class)
+class NotesScreenTest {
+
+
+}
---
+++ app/src/androidTest/java/xyz/teamgravity/notetaker/injection/ApplicationModuleTest.kt
@@ -0,0 +1,48 @@
+package xyz.teamgravity.notetaker.injection
+
+import android.app.Application
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import xyz.teamgravity.notetaker.feature_note.data.data_source.NoteDao
+import xyz.teamgravity.notetaker.feature_note.data.data_source.NoteDatabase
+import xyz.teamgravity.notetaker.feature_note.data.repository.NoteRepository
+import xyz.teamgravity.notetaker.feature_note.domain.repository.NoteRepositoryImpl
+import xyz.teamgravity.notetaker.feature_note.domain.use_case.AddNoteUseCase
+import xyz.teamgravity.notetaker.feature_note.domain.use_case.DeleteNoteUseCase
+import xyz.teamgravity.notetaker.feature_note.domain.use_case.GetNoteUseCase
+import xyz.teamgravity.notetaker.feature_note.domain.use_case.GetNotesUseCase
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ApplicationModuleTest {
+
+    @Provides
+    @Singleton
+    fun provideNoteDatabase(app: Application): NoteDatabase =
+        Room.inMemoryDatabaseBuilder(app, NoteDatabase::class.java)
+            .fallbackToDestructiveMigration()
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideNoteDao(db: NoteDatabase) = db.noteDao()
+
+    @Provides
+    fun provideNoteRepository(noteDao: NoteDao): NoteRepository = NoteRepositoryImpl(noteDao)
+
+    @Provides
+    fun provideGetNotesUseCase(noteRepository: NoteRepository): GetNotesUseCase = GetNotesUseCase(noteRepository)
+
+    @Provides
+    fun provideDeleteNoteUseCase(noteRepository: NoteRepository): DeleteNoteUseCase = DeleteNoteUseCase(noteRepository)
+
+    @Provides
+    fun provideAddNoteUseCase(noteRepository: NoteRepository): AddNoteUseCase = AddNoteUseCase(noteRepository)
+
+    @Provides
+    fun provideGetNoteUsaCase(noteRepository: NoteRepository): GetNoteUseCase = GetNoteUseCase(noteRepository)
+}
